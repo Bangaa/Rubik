ec52b7c1eba335a9661b4363fbf146c75e108d1f
Ian Mejias <idmconejeros@gmail.com>
2017-05-29 22:20:05 -0400

    Se implementa el programa con argumentos

--- alg.py
+++ alg.py
@@ -1,16 +1,20 @@
 
 import MatEncriptor as ME
 import ConversionTable as CT
+import sys
 
-txt = raw_input("Texto: ")
+if len(sys.argv) == 1:
+    filename = raw_input("Archivo entrada: ")
+    clave = raw_input("Clave (key): ")
+elif len(sys.argv) == 3:
+    filename = sys.argv[1]
+    clave = sys.argv[2]
 
-ct = CT.ConversionTable("Murcielago")
-ct2 = CT.ConversionTable("qwertyuioasdfghjklzxcvbnm")
-
+ct = CT.ConversionTable(clave)
 enc = ME.MatEncriptor(ct)
-enc2 = ME.MatEncriptor(ct2)
 
-me = enc.encriptar(txt)
-me2 = enc2.encriptar(txt)
-print me
-print me2
+try:
+    enc_txt = enc.encriptar(open(filename).read())
+    print enc_txt
+except IOError as ioe:
+    print "ERROR: No se encuentra el archivo '%s'" % (filename)
